88ace256f322f9d7143b06240f4d23368d9df595
Pavel Bedyaev <pavel@bedyaev.com>
2023-10-23 23:20:20 +0300

    hw 2 completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -1,12 +1,54 @@
 package hw02unpackstring
 
+//package main
+
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
+//	func main() {
+//		s, err := Unpack("aasdф2ы\\3yй5s2qq0w")
+//		//s, err := Unpack("a4bc2d5ef")
+//		fmt.Println(s, err)
+//	}
+func Unpack(input string) (string, error) {
 	// Place your code here.
-	return "", nil
+	result := ""
+	runes := []rune(input)
+	for i := 0; i < len(runes); i++ {
+		switch {
+		// first+ rune expected not to have +1 and +2 positions as numbers
+		case i+2 < len(runes) && unicode.IsDigit(runes[i+1]) && unicode.IsDigit(runes[i+2]):
+			message := "Digits are not allowed, only numbers [0-9]:" + string(runes[i+1]) + string(runes[i+2])
+			return message, ErrInvalidString
+
+		// first+ rune is not a number, keep it as is
+		case i+1 < len(runes) && !unicode.IsDigit(runes[i]) && !unicode.IsDigit(runes[i+1]):
+			result = result + string(runes[i])
+
+		// +0 and +1 runes are notDigit and isDigit, so repeat +0 rune +1 times
+		case i+1 < len(runes) && !unicode.IsDigit(runes[i]) && unicode.IsDigit(runes[i+1]):
+			times, _ := strconv.Atoi(string(runes[i+1]))
+			result = result + strings.Repeat(string(runes[i]), times)
+			i = i + 1 // skip rune with times itself
+
+		// first rune not a number
+		case i == 0:
+			check, err := strconv.Atoi(string(runes[i]))
+			if err == nil {
+				message := "First rune cannot be a number:" + strconv.Itoa(check)
+				return message, ErrInvalidString
+			}
+
+		// if last rune is not a number - keep it as is
+		case i+1 == len(runes) && !unicode.IsDigit(runes[i]):
+			result = result + string(runes[i])
+		}
+	}
+	return result, nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -17,10 +17,12 @@ func TestUnpack(t *testing.T) {
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
+		//{input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
 		// {input: `qwe\\5`, expected: `qwe\\\\\`},
 		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: "aasdф2ы\\3yй5s2qq0w", expected: "aasdффы\\\\\\yйййййssqw"},
 	}
 
 	for _, tc := range tests {
