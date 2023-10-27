58a0a83da8e670a30061e81817531021082a8ebc
Pavel Bedyaev <pavel@bedyaev.com>
2023-10-27 22:10:02 +0300

    first attempt

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,22 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
+func Top10(input string) []string {
 	// Place your code here.
+	tmp_slice := strings.Fields(input)
+	word_count := make(map[string]int)
+	for _, word := range tmp_slice {
+		//key, ok := word_count[word]
+		word_count[word]++
+	}
+	sort.Slice(word_count, func(i, j int) bool {
+		return word_count[i] < word_count[j]
+	})
+	fmt.Println(word_count)
 	return nil
 }
