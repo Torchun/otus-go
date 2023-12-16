ecb64893c26d1892eb0ca2a3c7e118dd32ea6a07
Pavel Bedyaev <pavel@bedyaev.com>
2023-12-16 13:07:06 +0300

    env reader

--- hw08_envdir_tool/env_reader_test.go
+++ hw08_envdir_tool/env_reader_test.go
@@ -1,7 +1,20 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
 
 func TestReadDir(t *testing.T) {
-	// Place your code here
+	t.Run("env cases: empty & unset & hello", func(t *testing.T) {
+		mapEnv, err := ReadDir("testdata/env")
+		if err != nil {
+			fmt.Println(fmt.Errorf("read env dir error: %w", err))
+			return
+		}
+		require.Equal(t, mapEnv["EMPTY"].Value, "")
+		require.Equal(t, mapEnv["UNSET"].NeedRemove, true)
+		require.Equal(t, mapEnv["HELLO"].Value, "\"hello\"")
+	})
 }
