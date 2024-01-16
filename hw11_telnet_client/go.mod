531dad10a63cd7c0e34f54a42df0bee7036fb107
Pavel Bedyaev <pavel@bedyaev.com>
2024-01-16 19:21:34 +0300

    HW11 done

--- hw11_telnet_client/go.mod
+++ hw11_telnet_client/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw11_telnet_client
+module github.com/Torchun/otus-go/hw11_telnet_client
 
 go 1.19
 
--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,108 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+var timeout time.Duration
+
+// Optional flag, will be executed on import, can be overwritten by "--timeout=5s" flag
+func init() {
+	flag.DurationVar(&timeout, "timeout", 10, "default timeout duration")
+}
 func main() {
-	// Place your code here,
-	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
+	flag.Parse()
+	// ["go-telnet", "--timeout=5s", "localhost", "4242"]
+	port := os.Args[len(os.Args)-1]
+	host := os.Args[len(os.Args)-2]
+
+	in := &bytes.Buffer{}
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT, os.Interrupt)
+	defer stop()
+
+	client := NewTelnetClient(host+":"+port, timeout, io.NopCloser(in), os.Stdout)
+	defer client.Close()
+
+	err := client.Connect()
+	if err != nil {
+		fmt.Printf("connection failed: %s\n", err)
+		return
+	}
+
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+	go tcpsend(ctx, stop, &wg, in, client)
+	go conncontrol(ctx, &wg, client)
+
+	wg.Wait()
+}
+
+func tcpsend(ctx context.Context, stop context.CancelFunc, wg *sync.WaitGroup, in *bytes.Buffer, t TelnetClient) {
+	// wait until context stopped
+	go func() {
+		<-ctx.Done()
+		// fmt.Println("ctx.Done at tcpsend")
+		// close connection to server manually
+		t.Close()
+		wg.Done()
+	}()
+
+	reader := bufio.NewReader(os.Stdin)
+	// infinite read from stdin and pass to server line by line
+	for {
+		resp, err := reader.ReadString('\n')
+		// fmt.Println(resp, err)
+		if err != nil {
+			fmt.Println(err)
+			// pass signal to close ctx
+			stop()
+			return
+		}
+
+		in.WriteString(resp)
+		err = t.Send()
+		if err != nil {
+			fmt.Println(err)
+			//stop()
+			return
+		}
+	}
+}
+
+func conncontrol(ctx context.Context, wg *sync.WaitGroup, t TelnetClient) {
+	// to perform select case and wait for interrupt/error
+	errCh := make(chan error)
+	defer func() {
+		wg.Done()
+	}()
+
+	for {
+		select {
+		case <-ctx.Done():
+			//fmt.Println("ctx.Done() poped")
+			// main interrupted by syscall (Ctrl+C)
+			// deferred wg.Done() executed (wg delta -1)
+			return
+		case errCh <- t.Receive():
+			err := <-errCh
+			if err != nil {
+				fmt.Println(err)
+				// io.Copy completed due to EOF/Error
+				// deferred wg.Done() executed (wg delta -1)
+				return
+			}
+		}
+	}
 }
